c06faf50d879871d5d390907374dac5e563a8f89
erik ben heckman <erik@heckman.ca>
2018-12-13 13:42:44 -0800

    Revert RandomCat addition, now has its own branch.

--- src/Plant.java
+++ src/Plant.java
@@ -8,7 +8,8 @@ import java.util.stream.Collectors;
 class Plant {
 
   //  private static final int STEP_SECONDS = 60 * 60 * 24; // one day
-  private static final int STEP_SECONDS = 20; // for testing
+  //private static final int STEP_SECONDS = 3; // for testing
+  private static final int STEP_SECONDS = 8; // for presentation
   static final String STEP_STRING = "day";
 
   private Instant born;
